cc108a8d3a6ce3bba48e7e1425bfb715d4dbbf4d
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-12-30 15:13:03 -0300

    Início do projeto.

---
+++ src/main/java/bootstrap/baseDeDados/BaseDeDadosBootstrap/BaseDeDadosBootstrapApplication.java
@@ -0,0 +1,13 @@
+package bootstrap.baseDeDados.BaseDeDadosBootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDeDadosBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDeDadosBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootstrap/baseDeDados/BaseDeDadosBootstrap/BaseDeDadosBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package bootstrap.baseDeDados.BaseDeDadosBootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDeDadosBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
